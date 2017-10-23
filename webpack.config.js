f130f01a5eae449d089a24cc49283d329bcdda4c
Garman_SZ <973714664@qq.com>
2017-10-23 21:56:56 +0800

    postcss-px2rem
    postcss-px2rem

--- webpack.config.js
+++ webpack.config.js
@@ -6,14 +6,19 @@ let webpack = require('webpack'),
     sourcePath = __dirname;
 // ==>
 //
-const vuxLoader = require('vux-loader')
-
+// const vuxLoader = require('vux-loader')
+// var autoprefixer = require('autoprefixer');
+// var px2rem = require('postcss-pxtorem');
+// var px2remOpts = {
+//     rootValue: 100,
+//     propWhiteList: [],
+// };
 
 // 原来的 module.exports 代码赋值给变量 webpackConfig，
 let config = {
     devtool: 'source-map',
     entry: [
-      path.join(sourcePath, 'src/index.ts')
+        path.join(sourcePath, 'src/index.ts')
     ],
     output: {
         path: path.join(sourcePath, '/dist'),
@@ -26,33 +31,28 @@ let config = {
         }, {
             test: /\.ts$/,
             use: 'ts-loader'
-        },
-        {
+        }, {
             test: /\.(html|htm)$/,
             use: 'raw-loader'
-        },
-        {
+        }, {
             test: /\.scss$/,
             use: ExtractTextPlugin.extract({
-                use: ['css-loader', 'sass-loader'],
+                use: ['css-loader', 'postcss-loader','sass-loader'],
                 fallback: 'style-loader'
             })
-        },
-        {
+        }, {
             test: /\.less$/,
             use: ExtractTextPlugin.extract({
                 use: ['css-loader', 'less-loader'],
                 fallback: 'style-loader'
             })
-        },
-        {
+        }, {
             test: /\.css$/,
             use: ExtractTextPlugin.extract({
                 use: ['css-loader'],
                 fallback: 'style-loader'
             })
-        },
-        {
+        }, {
             test: /\.(png|svg|jpg|gif)$/,
             use: ['file-loader']
         }]
@@ -60,12 +60,27 @@ let config = {
     resolve: {
         extensions: ['.ts', '.js'],
         alias: {
-            'vue$': path.resolve(__dirname,'node_modules/vue/dist/vue.esm.js')
+            'vue$': path.resolve(__dirname, 'node_modules/vue/dist/vue.esm.js')
         }
     },
     plugins: [
         // new webpack.ProvidePlugin({
         //     mui: "src/mui/mui.min.js"
+        // }),
+        new webpack.optimize.UglifyJsPlugin({
+            compressor: {
+                warnings: false
+            }
+        }),
+        // new webpack.LoaderOptionsPlugin({
+        //     // test: /\.xxx$/, // may apply this only for some modules
+        //     // postcss平台
+        //     // 此处配置了免前缀功能和px转换为rem功能
+        //     options: {
+        //         postcss: [autoprefixer({
+        //             browsers: ['last 2 versions']
+        //         }), px2rem(px2remOpts)],
+        //     }
         // }),
         new HtmlWebpackPlugin({
             template: path.join(sourcePath, 'src/index.tpl.html'),
@@ -89,5 +104,5 @@ let config = {
 };
 
 //即将原来的module.exports 改为 const webpackConfig
-module.exports = vuxLoader.merge(config, { plugins: ['vux-ui'] })
-//module.exports = config;
+// module.exports = vuxLoader.merge(config, { plugins: ['vux-ui'] })
+module.exports = config;
